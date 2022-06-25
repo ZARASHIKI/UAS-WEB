be0e53e7a4152fb78a1b8bce66a6eaaa088e5055
ZARASHIKI <agusyusup69@gmail.com>
2022-06-25 23:05:50 +0800

    new

--- about.php
+++ about.php
@@ -1,6 +1,16 @@
-<div class="centered">
+<?php include "header.php";?>
+<div class="left"style="text-align: justify;">
+<a>OUR JOURNEY</a>
+<hr class="my-4">
+<h1>ABOUT US</h1> 
  <p>
- Sepatu mungkin bukan barang asing bagi kita. Setiap pergi ke sekolah atau kantor kita pasti mengenakan sepatu. Selain untuk melindungi kaki, sepatu juga membuat penampilan kita menjadi lebih rapi. Namun sepatu ternyata juga menyimpan sejarah panjang dan menarik. Para ahli sejarah memperkirakan sepatu pertama kali dibuat pada zaman es atau lima juta tahun lalu dan dibuat dari kulit binatang. Sepatu primitif dalam jumlah besar pernah ditemukan di pedalaman Missouri, Amerika Serikat dan diperkirakan berasal dari 8000 Sebelum Masehi. Sepatu lainnya juga pernah ditemukan di pegunungan Perancis dan berasal dari 3300 SM. Selain dari kulit binatang ada juga sepatu yang terbuat dari rumput atau semak untuk melindungi kaki dari teriknya sinar matahari atau dinginnya suhu udara. Selain di Missouri Amerika, juga ditemukan alas kaki yang berasal dari peradaban Mesir kuno, Viking, dan China kuno. Pernah ditemukan sebuah alas kaki yang berasal dari Mesir kuno dan memiliki gambar yang sangat indah. Alas kaki tersebut merupakan milik Raja Mesir. Alas kaki saat itu digunakan juga untuk menunjukkan status sosial. Masyarakat Yunani kuno juga memiliki alas kaki yang sangat menunjang aktivitas mereka. Sepatunya memiliki banyak tali yang diikat di sekeliling betis. Tentara Romawi kuno juga memiliki sepatu yang sangat khas dan disebut caligae. Saat para tentara Romawi kembali dari peperangan dan menang, caligae nantinya akan diberi paku yang berasal dari perunggu, perak bahkan emas.
-Kini 'SEPATOS' hadir dengan material yang premium (jelly, kulit) yang sangat nyaman digunakan saat dipakai bepergian jauh. Sepatos dengan banyak pilihan size tentunya sangat cocok dengan semua kalangan (unisex).
+ Sepatu mungkin bukan barang asing bagi kita. Setiap pergi ke sekolah atau kantor kita pasti mengenakan sepatu. Selain untuk melindungi kaki, sepatu juga membuat penampilan kita menjadi lebih rapi. Namun sepatu ternyata juga menyimpan sejarah panjang dan menarik. Para ahli sejarah memperkirakan sepatu pertama kali dibuat pada zaman es atau lima juta tahun lalu dan dibuat dari kulit binatang.
+ </p>
+ <p>
+ Sepatu primitif dalam jumlah besar pernah ditemukan di pedalaman Missouri, Amerika Serikat dan diperkirakan berasal dari 8000 Sebelum Masehi. Sepatu lainnya juga pernah ditemukan di pegunungan Perancis dan berasal dari 3300 SM. Selain dari kulit binatang ada juga sepatu yang terbuat dari rumput atau semak untuk melindungi kaki dari teriknya sinar matahari atau dinginnya suhu udara. Selain di Missouri Amerika, juga ditemukan alas kaki yang berasal dari peradaban Mesir kuno, Viking, dan China kuno. Pernah ditemukan sebuah alas kaki yang berasal dari Mesir kuno dan memiliki gambar yang sangat indah. Alas kaki tersebut merupakan milik Raja Mesir.
+ </p>
+ <p>
+ Alas kaki saat itu digunakan juga untuk menunjukkan status sosial. Masyarakat Yunani kuno juga memiliki alas kaki yang sangat menunjang aktivitas mereka. Sepatunya memiliki banyak tali yang diikat di sekeliling betis. Tentara Romawi kuno juga memiliki sepatu yang sangat khas dan disebut caligae. Saat para tentara Romawi kembali dari peperangan dan menang, caligae nantinya akan diberi paku yang berasal dari perunggu, perak bahkan emas.Kini 'SEPATOS' hadir dengan material yang premium (jelly, kulit) yang sangat nyaman digunakan saat dipakai bepergian jauh. Sepatos dengan banyak pilihan size tentunya sangat cocok dengan semua kalangan (unisex).
  </p>
 </div>
+<div class="right"><img src="upload/8.jpeg" alt="Snow" style="width: 100%;opacity:70% ;height: 500px;"></div>
