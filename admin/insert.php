e2a12a8746617c2ed02b2a06574dfe77c49ceb26
MonalisaDN <monalisadennatan28@gmail.com>
2022-06-25 22:55:04 +0800

    admin

--- admin/insert.php
+++ admin/insert.php
@@ -1,14 +1,38 @@
 <?php
-include "header.php";
+include "header.php"
 ?>
-   <div class="card">
-       <h2>Artikel baru</a>
-       <form action="index.php" method="post" enctype="multipart/form-data">
-           <input type="hidden" name="ngapain" value="insert">
-           <input type="text" name="title"><br>
-           <input type="file" name="image"><br>
-           <textarea name="content" rows="10"></textarea><br>
-           <button type="submit">Kirim</button>
-       </form>
-   </div>
+       
+<div class="row">
+  <div class="column">
+  <div class="card">
+  
+    <a>Produk Baru</a>
+    <table>
+    <form action="index.php" method="post" enctype="multipart/form-data">
+        <tr>
+        <input type="hidden" name="ngapain" value="insert"></tr>
+			<tr>
+				<td>Nama</td>
+				<td><input type="text" name="title"></td>					
+			</tr>	
+			<tr>
+				<td>Gambar </td>
+				<td style = "position: relative; left: 50px;"><input type="file" name="image"></td>					
+			</tr>
+			<tr>
+				<td>Content</td>
+				<td><textarea name="content" rows = "10"></textarea></td>					
+			</tr>
+            <tr>
+				<td>Harga</td>
+				<td><input type="int" name="harga"></td>		
+			</tr>
+            <tr>
+				<td></td>
+				<td><input type="submit" value="Simpan"></td>		
+			</tr>
+            </form>		
+		</table> 
+       </div>
+   
 
