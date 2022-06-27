6b235e9cb8a6a971da14e5e4d6b74c7d4bd530b1
ZARASHIKI <agusyusup69@gmail.com>
2022-06-27 17:15:53 +0800

    new

--- admin/insert.php
+++ admin/insert.php
@@ -1,20 +1,6 @@
 <?php
 include "header.php"
 ?>
-<<<<<<< HEAD
-<div class ="row">
-   <div class= "card"> 
-       <h2>Tambah Baru</a>
-       <form action="index.php" method="post" enctype="multipart/form-data">
-           <input type="hidden" name="sepatu" value="insert">
-           <input type="text" name="title"><br>
-           <input type="text" name="price"><br>
-           <input type="file" name="image"><br>
-           <textarea name="content" rows="10"></textarea><br>
-           <button type="submit">Submit</button>
-       </form>
-   </div>
-=======
        
   <div class="card">
   
@@ -47,5 +33,4 @@ include "header.php"
 		</table> 
        </div>
    
->>>>>>> upstream/main
 
