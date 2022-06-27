e53b3fb8dcd50191c4a62f440720e958e7c85711
ayutantyana19 <ayutantyana19@gmail.com>
2022-06-27 17:05:02 +0800

    footer

--- footer.php
+++ footer.php
@@ -3,7 +3,6 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>UAS</title>
     <link rel="stylesheet" href="123.css">
     <link rel="stylesheet" href="art.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
