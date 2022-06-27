bea02751ae8d2cf2c98fd239f0cd4bda44ab41b1
ZARASHIKI <agusyusup69@gmail.com>
2022-06-27 17:11:50 +0800

    new

--- admin/index.php
+++ admin/index.php
@@ -14,33 +14,6 @@
 </div>
 </div>
 <?php
-<<<<<<< HEAD
-include "header.php";
-<div class="">
-<img src="upload/bcd.jpeg" alt="Snow" style="width: 100%;opacity:70% ;height: 500px;">
-  <div class="centered" >
-  <div class="jumbotron">
-  <div class="tcolor">
-  <h1 class="display-4">SEPATOS.ID</h1>
-  <p class="lead"style="font-size: 16pt;">Gayakan Style Mu Dengan SEPATOS </p>
-  <hr class="my-4">
-  <p style="font-size: 14pt;">Pakailah Sepatu Untuk Melindungi Kaki Saat Berkendara</p>
-  <p></p>
-  </div>
-</div>
-</div>    
-</div>
-</div>
- 
-if(isset($_POST["ngapain"]) && $_POST["ngapain"] == 'insert') {   $target_dir = "../upload/";
-   $target_file = $target_dir . basename($_FILES["image"]["name"]);
-   move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
-   $title = $_POST['title'];
-   $content = $_POST['content'];
-   $image = $_FILES["image"]["name"];
-   $price = $_POST['price'];
-   $conn->query("INSERT INTO Post set title='$title', content='$content', image='$image', price='$price'");
-=======
 if(isset($_POST["ngapain"]) && $_POST["ngapain"] == 'insert')
 
 if($_POST["ngapain"] == 'insert') {
@@ -65,7 +38,6 @@ if($_POST["submit"] == 'update'){
     $content = $_POST['content'];
     $harga = $_POST['harga'];
     $conn->query("UPDATE Post set title='$title',content='$content', image='$gmb', harga = '$harga' WHERE id='$id'");
->>>>>>> upstream/main
 }
 if(isset($_POST["belajar"]) && $_POST["belajar"] == 'update') {   $target_dir = "../upload/";
   $target_file = $target_dir . basename($_FILES["image"]["name"]);
@@ -80,18 +52,6 @@ if(isset($_POST["belajar"]) && $_POST["belajar"] == 'update') {   $target_dir = 
  
 $posts = $conn->query("SELECT * FROM Post");
 ?>
-<<<<<<< HEAD
-<a href="insert.php">Buat artikel</a>
-<?php
-while($post = $posts->fetch_object()) {
- ?>
-   <div class="card">
-       <a href="../post.php?id=<?php echo $post->id ?>"><h2><?php echo $post->title; ?></h2></a>
-       <h5><?php echo $post->createdAt; ?></h5>
-       <div><img src="../uploads/<?php echo $post->image?>"width="100%"></div>
-       <a href="update.php?id=<?php echo $post->id ?>">Edit</a>
-       <a href="delete.php?id=<?php echo $post->id ?>">delete</a>
-=======
 
 <?php
 while($post = $posts->fetch_object()) {
@@ -107,7 +67,6 @@ while($post = $posts->fetch_object()) {
        <a class="btn btn-outline-light" style = "background-color: #876445;" href="update.php?id=<?php echo $post->id ?>">Edit</a>
        <a class="btn btn-outline-light" style = "background-color: #876445;" href="delete.php?id=<?php echo $post->id ?>" onclick ="return confirm('Apakah kamu yakin akan menghapus?')">Delete</a>
    </div>
->>>>>>> upstream/main
    </div>
 </div>
    <?php
